caf44c7aaba754a9678a8b0661473f92310ab90a
Jared Grippe <tech_ops+jared@change.org>
2012-07-07 20:23:48 -0700

    WIP

--- spec/integration/full_test_run_spec.rb
+++ spec/integration/full_test_run_spec.rb
@@ -6,8 +6,23 @@ describe "a full hobson test run" do
 
   it "should work" do
 
+    Hobson::Project.create(origin:)
 
+    # create project
+    # create test_run
+    # Resque.work!
+      # setup workspace
+      # lookup testrun
+      # checkout project
+      # prepare project
+      # ask project for a list of tests
+      # create & schedule test_run jobs
 
+    # ensure test records were created
+    # ensure job records were created
+    # ensure jobs were scheduled
+
+    # Resque.work!
 
 
 
---
+++ spec/workspace.rb
@@ -0,0 +1,3 @@
+module Workspace
+
+end
