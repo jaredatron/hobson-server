5414bb86715c3f101bb8d6253cc5f149cf4ec08f
Jared Grippe <tech_ops+jared@change.org>
2012-09-16 03:35:16 -0700

    refactored spec/hobson/server_spec.rb

--- spec/hobson/server_spec.rb
+++ spec/hobson/server_spec.rb
@@ -11,33 +11,36 @@ describe Hobson::Server do
   alias_method :response, :last_response
 
 
-  context "when there are no projects" do
-    describe "get" do
-      it "should return an empty set" do
-        get '/projects'
-        response.headers["Content-Type"].should == 'application/json;charset=utf-8'
-        json = JSON.parse response.body
-        json.should == {
-          "projects" => []
-        }
-      end
-    end
-  end
 
-  context "when there are 2 projects" do
-    before do
-      @projects = [
-        Hobson::Project.create(origin: 'git://github.com/soveran/ohm.git'),
-        Hobson::Project.create(origin: 'git://github.com/rails/rails.git'),
-      ]
+
+  describe "get /projects" do
+
+    def get!
+      get '/projects'
+      response.headers["Content-Type"].should == 'application/json;charset=utf-8'
+      response.body.should == {"projects" => projects.map(&:attributes)}.to_json
     end
-    describe "get" do
-      it "should return those 2 projects" do
-        get '/projects'
-        response.headers["Content-Type"].should == 'application/json;charset=utf-8'
-        response.body.should == {"projects" => @projects.map(&:attributes)}.to_json
+
+    context "when there are no projects" do
+      let(:projects){ [] }
+
+      it "should return an empty set" do
+        get!
       end
     end
+
+    context "when there are 2 projects" do
+      let!(:projects) do
+        [
+          Hobson::Project.create(origin: 'git://github.com/soveran/ohm.git'),
+          Hobson::Project.create(origin: 'git://github.com/rails/rails.git'),
+        ]
+      end
+      it "should return those 2 projects" do
+        get!
+      end
+    end
+
   end
 
 end
