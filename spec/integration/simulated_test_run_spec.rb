de88346bbfa86217dfe730be15b1450124e4267e
Jared Grippe <tech_ops+jared@change.org>
2012-09-17 21:35:00 -0700

    WIP

--- spec/integration/simulated_test_run_spec.rb
+++ spec/integration/simulated_test_run_spec.rb
@@ -147,7 +147,7 @@ describe Hobson::Server do
     })
 
 
-    # updating the status of a job
+    # track an event on job 0
     post "/test_runs/1/jobs/0/events", {
       "event" => {
         "description" => "checking out code",
@@ -155,6 +155,18 @@ describe Hobson::Server do
       }
     }
     response.status.should == 200
+
+    # confirm changes to job 0
+    get "/test_runs/1"
+    response_data["test_run"]["jobs"][0].should == j({
+      "index" => "0",
+      "events" => [
+        {
+          "description" => "checking out code",
+          "occurred_at" => "2012-09-17 18:13:02 -0700",
+        }
+      ]
+    })
 
     # started a test
     put "/test_runs/1", {
@@ -170,67 +182,7 @@ describe Hobson::Server do
     response.status.should == 200
 
 
-    get "/test_runs/1"
-    response_data.should == j({
-      "test_run" => {
-        "id"             => "1",
-        "project_origin" => "git@github.com:deadlyicon/hobson-server.git",
-        "sha"            => "12321321321321",
-        "requestor"      => "Jared Grippe",
-        "created_at"     => now,
-        "jobs" => [
-          {
-            "index" => "0",
-            "events" => [
-              {
-                "description" => "checking out code",
-                "occurred_at" => "2012-09-17 18:13:02 -0700",
-              }
-            ]
-          },
-          {
-            "index"  => "1",
-            "events" => [],
-          },
-        ],
-        "tests" => [
-          {
-            "uuid"         => "spec:models/user_spec.rb",
-            "job_index"    => "0",
-            "started_at"   => "2012-09-17 18:13:02 -0700",
-            "completed_at" => nil,
-            "result"       => nil,
-            "tries"        => nil,
-          },{
-            "uuid"         => "spec:models/post_spec.rb",
-            "job_index"    => "0",
-            "started_at"   => nil,
-            "completed_at" => nil,
-            "result"       => nil,
-            "tries"        => nil,
-          },{
-            "uuid"         => "scenario:I should be able to see my first post",
-            "job_index"    => "1",
-            "started_at"   => nil,
-            "completed_at" => nil,
-            "result"       => nil,
-            "tries"        => nil,
-          },{
-            "uuid"         => "scenario:I should be able to delete my first post",
-            "job_index"    => "1",
-            "started_at"   => nil,
-            "completed_at" => nil,
-            "result"       => nil,
-            "tries"        => nil,
-          },
-        ]
-      }
-    })
-
-
-
-
-
+    # confirm changes to test 0
     get "/test_runs/1"
     response_data["test_run"]["tests"][0].should == j({
       "uuid"         => "spec:models/user_spec.rb",
@@ -241,6 +193,7 @@ describe Hobson::Server do
       "tries"        => nil,
     })
 
+    # complete the test
     put "/test_runs/1", {
       "test_run" => {
         "tests" => [
@@ -255,7 +208,7 @@ describe Hobson::Server do
     }
     response.status.should == 200
 
-
+    # confirm changes to test 0
     get "/test_runs/1"
     response_data["test_run"]["tests"][0].should == j({
       "uuid"         => "spec:models/user_spec.rb",
@@ -265,23 +218,6 @@ describe Hobson::Server do
       "result"       => 'PASS',
       "tries"        => "1",
     })
-
-    # simulate requesting a test run
-
-      # POST /projects/git@github.com%2Frails%2Frails/test_runs
-      # should create a project
-      # should create a project test run
-
-    # simulate building a test run
-
-      # PUT /projects/git@github.com%2Frails%2Frails/test_runs/1
-      # PUT /projects/git@github.com%2Frails%2Frails/test_runs/tests
-      # should update the test run
-
-    # simulate running a test run
-
-
-
 
   end
 
