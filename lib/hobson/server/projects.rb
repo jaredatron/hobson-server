71559ea14c30a38d5d899dfc76bcb163539209fd
Jared Grippe <tech_ops+jared@change.org>
2012-09-17 22:02:57 -0700

    WIP

--- lib/hobson/server/projects.rb
+++ lib/hobson/server/projects.rb
@@ -26,28 +26,5 @@ class Hobson::Server
       end
     end
 
-    #   # read
-    #   get do
-    #     @project.to_json
-    #   end
-
-    #   # delete
-    #   delete do
-    #     if @project.nil?
-    #       status 404
-    #     else
-    #       @project.delete
-    #     end
-    #     return nil
-    #   end
-
-    #   require 'hobson/server/projects/tests'
-    #   namespace '/tests', &Hobson::Server::Projects::Tests
-
-    #   require 'hobson/server/projects/test_runs'
-    #   namespace '/test_runs', &Hobson::Server::Projects::TestRuns
-
-    # end
-
   end
 end
